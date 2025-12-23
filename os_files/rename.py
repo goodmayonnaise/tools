fcdad749c9ca8972335e14bf5f162039ab2810ff
JIYEON JEON <85322193+goodmayonnaise@users.noreply.github.com>
2025-12-23 16:49:30 +0900

    Add script to rename files in specified directories

---
+++ os_files/rename.py
@@ -0,0 +1,14 @@
+import os, shutil
+
+
+if __name__ == "__main__":
+    path = r""
+    dirs = ['CL', 'DK', 'False', 'Repair']
+    for dir in dirs:
+        dirnums = os.listdir(os.path.join(path, dir))
+        for dirnum in dirnums:
+            fnames = os.listdir(os.path.join(path, dir, dirnum))
+            
+            for fname in fnames:
+                shutil.move(os.path.join(path, dir, dirnum, fname), os.path.join(path, dir, dirnum, f"{dir}_{fname}"))
+            
