566712de960b3003a593f8e19214e99d2d5aa39b
jiyeon.jeon <jeonjiy2on@gmail.com>
2026-01-14 10:46:38 +0900

    rename

---
+++ visualization/save_result_png_3d.py
@@ -0,0 +1,248 @@
+
+import cv2, pickle, yaml, os
+import numpy as np 
+from tqdm import tqdm
+from einops import rearrange
+
+from model import new_model
+from utils import set_device
+
+
+import torch
+from torch import nn
+from torch.optim import lr_scheduler, Adam
+import torchvision.transforms as T
+
+
+def point_padding(wh_list, y_img, H, W, fill_size = 1):        
+    for h, w in zip(wh_list[0], wh_list[1]):
+        if fill_size<=h and h <=H-fill_size-1 and fill_size<=w and w<=W-fill_size-1:
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h+fill_size,w+fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w-fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w+fill_size] = y_img[:,h,w]
+        elif fill_size<=h and h <=H-fill_size-1 and fill_size>w:
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h+fill_size,w+fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w+fill_size] = y_img[:,h,w]
+        elif fill_size<=h and h <=H-fill_size-1 and w>W-fill_size-1:
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w-fill_size] = y_img[:,h,w]
+        elif fill_size>h and fill_size<=w and w<=W-fill_size-1:
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h+fill_size,w+fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w-fill_size] = y_img[:,h,w]
+        elif h > H-fill_size-1 and fill_size<=w and w<=W-fill_size-1:
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w-fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w+fill_size] = y_img[:,h,w]
+        elif fill_size>h and fill_size>w :
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h+fill_size,w+fill_size] = y_img[:,h,w]
+        elif fill_size>h and w>W-fill_size-1:
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h+fill_size,w] = y_img[:,h,w]
+            y_img[:,h+fill_size,w-fill_size] = y_img[:,h,w]
+        elif h > H-fill_size-1 and fill_size>w :
+            y_img[:,h,w+fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w+fill_size] = y_img[:,h,w]
+        elif h>H-fill_size-1 and w>W-fill_size-1:
+            y_img[:,h,w-fill_size] = y_img[:,h,w]
+            y_img[:,h-fill_size,w] = y_img[:,h,w]
+            y_img[:,h-fill_size,w-fill_size] = y_img[:,h,w]
+    return y_img
+
+
+def colorize_sample(d_arg, point_2D_from_3D_x, point_2D_from_3D_y, label, CFG):# d_arg는 swap dict가 끝난 클래스
+        color_dict = CFG["color_map"] # config내 color map bgr
+        max_sem_key = 0
+        for key, data in color_dict.items(): # max_sem_key 설정 , color_dict의 key에는 swap이 안된 클래스의 색상 data
+            if key + 1 > max_sem_key:
+                max_sem_key = key + 1 # 260개
+        sem_color_lut = np.zeros((max_sem_key + 100, 3), dtype=np.float32) 
+        for key, value in color_dict.items():
+            sem_color_lut[key] = np.array(value, np.float32) 
+        # print(sem_color_lut[0:20])
+        d_color_origin = np.zeros(shape=(d_arg.shape[0],d_arg.shape[1],3))
+        for x,y,l in zip(point_2D_from_3D_x, point_2D_from_3D_y, label):
+            d_color_origin[y][x]=sem_color_lut[l]
+
+        d_color=np.zeros(shape=(d_arg.shape[0],d_arg.shape[1],3))
+        for i in range(len(d_arg)):
+            for j in range(len(d_arg[i])):
+                d_color[i][j] = sem_color_lut[CFG["learning_map_inv"][d_arg[i][j]]]
+                
+        return d_color, d_color_origin # pred, uv_laebel 
+
+
+def replace_with_dict(ar, learning_map):
+    # Extract out keys and values
+    k = np.array(list(learning_map.keys()))
+    v = np.array(list(learning_map.values()))
+
+    # Get argsort indices
+    sidx = k.argsort()
+    
+    # Drop the magic bomb with searchsorted to get the corresponding
+    # places for a in keys (using sorter since a is not necessarily sorted).
+    # Then trace it back to original order with indexing into sidx
+    # Finally index into values for desired output.
+    return v[sidx[np.searchsorted(k,ar,sorter=sidx)]]    
+
+def load_config():
+    cfg_path = './config/semantic-kitti.yaml'
+    try:
+        print("Opening config file %s" % "config/semantic-kitti.yaml")
+        CFG = yaml.safe_load(open(cfg_path, 'r'))
+    except Exception as e:
+        print(e)
+        print("Error opening yaml file.")
+        quit()
+    return CFG 
+
+
+if __name__ == "__main__":
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID" 
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
+    gpus = os.environ["CUDA_VISIBLE_DEVICES"]
+    num_workers = len(gpus.split(",")) * 2
+    device = set_device(gpus) 
+    use_gpu = torch.cuda.is_available()
+    num_gpu = list(range(torch.cuda.device_count()))
+    
+    input_shape = (256, 1280)
+    
+    CFG = load_config()
+    learning_map = CFG['learning_map']
+    learning_map_inv = CFG['learning_map_inv']
+    color_map = CFG['color_map']
+    
+    transform = T.ToPILImage()
+
+    sequence_n = '00'
+    img_dir = f'/mnt/team_gh/kitti/dataset/sequences/{sequence_n}/image_2'
+    img_files = os.listdir(img_dir)
+    img_filenames = []
+    for img_file in img_files:
+        img_filenames.append(img_file[:-4])    
+    
+    for file_n in tqdm(img_filenames):
+        x_img = f'/mnt/team_gh/kitti/dataset/sequences/{sequence_n}/image_2/{file_n}.png'
+        x_rem = f'/mnt/team_gh/kitti/dataset/sequences/{sequence_n}/projection_front/remission/{file_n}.npy'
+        mapping_data = f'/mnt/team_gh/kitti/dataset/sequences/{sequence_n}/proj_point_label_mapping/{file_n}.pickle' # output y_img, y_rem 
+        
+        x_rem = np.load(x_rem)
+        x_rem = np.array(x_rem)
+        min_H = 0
+        for i in x_rem:
+            if sum(i!=-1):
+                break
+            min_H += 1
+        
+        H, W = input_shape[0], input_shape[1]
+        
+        x_img = cv2.imread(x_img)
+        x_img = np.array(x_img)
+        x_img = cv2.resize(x_img[min_H:,:], (W, H))
+        cv2.imwrite(f'./samples/rgb/{sequence_n}_{file_n}_rgb.png', x_img)
+        x_img = torch.FloatTensor(x_img)
+        x_img = rearrange(x_img, 'h w c -> c h w') # 20 256 1280
+        
+        x_rem_H, x_rem_W = x_rem.shape # 256 1280 
+        
+        x_rem = cv2.resize(x_rem[min_H:,:], (W,H))
+        x_rem = np.expand_dims(x_rem, axis=-1)
+        x_rem = torch.FloatTensor(x_rem)
+        x_rem = rearrange(x_rem, 'h w c -> c h w') # 20 256 1280
+        
+        with open(mapping_data, 'rb') as f:
+            y_map = pickle.load(f)
+        
+        v, u = y_map['uv']  # 19369
+        u, v = u*(W/x_rem_W), (v-min_H)*(H/(x_rem_H-min_H))
+        u, v = np.round(u,0).astype(np.int32), np.round(v,0).astype(np.int32)
+        
+        uv_label = np.array(y_map['label']) # n = 19369
+        
+        y_rem = np.zeros(shape=(20, H, W))
+        y_rem[replace_with_dict(uv_label, learning_map), v, u] = 1
+        
+        y_img = y_rem
+        wh_list = np.where(np.sum(y_rem, axis=0)!=0)
+        y_img = point_padding(wh_list, y_img, H, W, 2)
+        y_img[0, np.sum(y_img, axis=0)==0] = 1 # numpy 에서 cv2.imwrite
+        # y_color, y_color_origin = colorize_sample(np.argmax(y_img,axis=0), u, v, uv_label, CFG)  
+        # cv2.imwrite("./samples/label_fill2.png", y_color)
+        # cv2.imwrite('./samples/label_origin.png',y_color_origin)
+        y_img = torch.FloatTensor(y_img)
+        
+        y_rem[0, np.sum(y_rem, axis=0)==0] = 1
+        y_rem = rearrange(y_rem, 'c h w -> c (h w)')
+        y_rem = torch.FloatTensor(y_rem)
+        
+        x_img, x_rem, y_img, y_rem = x_img.unsqueeze(0), x_rem.unsqueeze(0), y_img.unsqueeze(0), y_rem.unsqueeze(0)
+        # x_img, x_rem, y_img, y_rem = x_img.to(device).unsqueeze(0), x_rem.to(device).unsqueeze(0), y_img.to(device).unsqueeze(0), y_rem.to(device).unsqueeze(0)
+        
+        # state = torch.load('./weights/best_weight/random_states_3.pkl')
+        state = torch.load('./weights/best_weight/pytorch_model.bin')
+        model = new_model(input_shape=input_shape)
+        # model = model.cuda()
+        # model = nn.DataParallel(model, device_ids=num_gpu)
+        # model.load_state_dict(state['model_state_dict'])
+        model.load_state_dict(state)
+        
+        optimizer = Adam(model.parameters(), lr=1e-4)
+        schdeuler = lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+        
+        with torch.no_grad():
+            model.eval()
+            segment_out, uv_out, uv = model(x_img, x_rem) 
+            segment_out, uv_out , uv = segment_out.squeeze(0), uv_out.squeeze(0), uv.squeeze(0) # 20 256 1280 / 20 256*1280
+            '''
+            segment out     20 256 1280
+            uv out          20 256*1280
+            uv              32 256 1280 
+            '''
+            segment_arr = segment_out.cpu().numpy()
+            segment_arr = np.argmax(segment_arr, axis=0)
+            
+            segment_result = np.zeros([*segment_arr.shape, 3])
+            
+            for c in color_map.keys():
+                segment_result[segment_arr==c] = color_map[c][::-1]
+            
+            cv2.imwrite(f"./samples/rgb_out/{sequence_n}_{file_n}_rgb_out.png", segment_result)
+
+            uv_arr = uv_out.cpu().numpy()
+            uv_arr = rearrange(uv_arr, 'c (h w) -> c h w',  h=256) # 20 256 1280
+            
+            uv_arr = np.argmax(uv_arr, axis=0) #  1 256 1280
+            
+            # print(uv_arr.max()) # 19 
+            # print(uv_arr.min()) # 1 
+            
+            result = np.zeros([H, W])
+            
+            for x, y in zip(u, v):
+                result[y, x] = uv_arr[y, x]
+            
+            uv_pred, uv_label = colorize_sample(result, u, v, uv_label, CFG)
+            
+            cv2.imwrite(f"./samples/uv_pred/{sequence_n}_{file_n}_pred.png", uv_pred)
+            cv2.imwrite(f'./samples/uv_label/{sequence_n}_{file_n}_uv_label.png', uv_label)
+        
---
+++ visualization/vis_heatmap_rgb.py
@@ -0,0 +1,207 @@
+'''
+    "configurations": [
+        {
+            "name": "heatmap",
+            "type": "debugpy",
+            "request": "launch",
+            "program": "${workspaceFolder}/heatmap2.py",
+            "args": [
+                "data/split2/val",
+                "--weight", "weights/efficientnet-b3-no-prt/model_best.pth.tar",
+            ],
+            "cwd": "${workspaceFolder}",
+            "justMyCode": false,
+            "console": "integratedTerminal"
+        }
+    ]
+'''
+
+import argparse
+import os
+import PIL.Image as Image
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+
+import numpy as np
+import cv2 
+
+from efficientnet_pytorch import EfficientNet
+
+class ImageFolderWithPath(ImageFolder):
+    def __getitem__(self, index):
+        image, target = super().__getitem__(index)
+        path, _ = self.samples[index]
+        return image, target, path
+    
+feature_maps = {}
+
+def save_feature(name):
+    def hook(module, input, output):
+        feature_maps[name] = output.detach()
+
+    return hook
+
+parser = argparse.ArgumentParser()
+parser.add_argument('data', type=str)
+parser.add_argument('--arch', default="efficientnet-b3")
+parser.add_argument('--weight', type=str, required=True)
+parser.add_argument('--batch-size', type=int, default=1)
+parser.add_argument('--gpu', type=int, default=0)
+# parser.add_argument('--out', type=str, default='heatmap')
+
+def main():
+    args = parser.parse_args()
+    args.out = os.path.join(*args.weight.split('/')[-1:], *args.data.split('/'))
+    os.makedirs(args.out, exist_ok=True)
+    model = EfficientNet.from_name(args.arch)
+    model._fc = nn.Linear(model._fc.in_features, 2)
+    model = nn.DataParallel(model)
+
+    checkpoint = torch.load(args.weight, map_location='cpu')
+    model.load_state_dict(checkpoint['state_dict'], strict=True)
+
+    torch.cuda.set_device(args.gpu)
+    model = model.cuda(args.gpu)
+    model.eval()
+
+    # vis feature setting
+    block_list = [2, 6, 12]
+    block_names = []
+    for idx in block_list:
+        model.module._blocks[idx].register_forward_hook(
+            save_feature(f"block{idx}")
+        )
+        block_names.append(f"block{idx}")
+    model.module._conv_head.register_forward_hook(save_feature("conv_head"))
+    block_names.append("conv_head")
+
+    cudnn.benchmark = True
+
+    image_size = EfficientNet.get_image_size(args.arch)
+    
+    transform = transforms.Compose([
+        transforms.Resize(image_size, interpolation=Image.BICUBIC),
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
+    dataset = ImageFolderWithPath(args.data, transform)
+    dataset.class_to_idx = {"defect":0, "false":1}
+
+    loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.batch_size,
+        shuffle=False
+    )
+    idx_to_class = {v: k for k, v in loader.dataset.class_to_idx.items()}
+
+
+    with torch.no_grad():
+        for image, target, path in loader:
+            image = image.cuda(args.gpu)
+
+            logits = model(image)
+            preds = logits.argmax(1)
+            
+            image, pred, target, path = image[0], preds[0], target[0], path[0]
+            
+            # path =
+            img = Image.open(path).convert("RGB")
+            img = np.array(img.resize((image_size, image_size)))
+            
+            results = [img]
+            
+            for bname in block_names:
+                
+                fmap = feature_maps[bname]
+                fmap = fmap.mean(1)
+                fmap = fmap.cpu().numpy()
+                
+                cam = fmap[0]
+                cam = np.maximum(cam, 0)
+                cam = cam / (cam.max() + 1e-6)
+                cam = cv2.resize(cam, (image_size, image_size))
+
+                heatmap = np.uint8(255 * cam)
+                heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+
+                overlay = cv2.addWeighted(img, 0.6, heatmap, 0.4, 0)
+                
+                # test
+                label = f"Layer: {bname}"
+                gt_label = f"GT: {idx_to_class[target.item()]}"
+                pred_label = f"Pred: {idx_to_class[pred.item()]}"
+
+                h, w, _ = overlay.shape
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                font_scale, thickness = 0.45, 1
+                (layer_w, layer_h), baseline1 = cv2.getTextSize(
+                    label, font, font_scale, thickness
+                )
+                (gt_w, gt_h), baseline2 = cv2.getTextSize(
+                    gt_label, font, font_scale, thickness
+                )
+                (pred_w, pred_h), baseline3 = cv2.getTextSize(
+                    pred_label, font, font_scale, thickness
+                )
+
+                x_layer, x_gt, x_pred = (w-layer_w)//2, (w-gt_w)//2, (w-pred_w)//2
+                y_pred = h - 5 
+                y_gt = y_pred - pred_h - 5
+                y_layer = y_gt - layer_h - 5
+
+                cv2.rectangle(
+                    overlay, 
+                    (x_layer-2, y_layer-layer_h-2),
+                    (x_layer+layer_w+2, y_layer+baseline1+2),
+                    (0,0,0), -1
+                )
+                cv2.rectangle(
+                    overlay, (x_gt-2, y_gt-gt_h-2),
+                    (x_gt+gt_w+2, y_gt+baseline2+2),
+                    (0,0,0), -1
+                )
+                cv2.rectangle(
+                    overlay,  (x_pred-2, y_pred-pred_h-2),
+                    (x_pred+pred_w+2, y_pred+baseline3+2),
+                    (0,0,0),-1
+                )
+                cv2.putText(
+                    overlay, label, (x_layer, y_layer),
+                    font, font_scale, (255,255,255), thickness,
+                    cv2.LINE_AA
+                
+                )
+                cv2.putText(
+                    overlay, gt_label, (x_gt,y_gt), font,
+                    font_scale, (255,255,255), thickness,
+                    cv2.LINE_AA
+                )
+                cv2.putText(
+                    overlay, pred_label, (x_pred,y_pred),
+                    font, font_scale, (255,255,255), thickness,
+                    cv2.LINE_AA
+                )
+                results.append(overlay[:, :, ::-1])
+
+            fname = os.path.basename(path)
+            out_path = os.path.join(args.out, fname)
+            results = np.concatenate(results, 1)
+            cv2.imwrite(out_path, results)
+            print(f"[SAVE] {out_path} | GT : {target.item()} PRED : {pred.item()}")
+
+
+    print("Done.")
+                
+
+
+if __name__ == "__main__":
+    main()
